d6fa164585dfe3217f503e450290e22288816089
liimonx <26011051+liimonx@users.noreply.github.com>
2026-02-22 14:11:03 +0000

    test: add unit tests for zip function in utility-api
    - Added `sass-true` and `jest` for testing SCSS functions.
    - Created `styles/02-tools/test/_tools.utility-api.test.scss` to test `zip` function.
    - Created `test/scss.test.js` runner.
    - Updated `package.json` with test script and dependencies.

---
+++ test/scss.test.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const glob = require('glob');
+const sassTrue = require('sass-true');
+
+describe('Sass', () => {
+  const sassTestFiles = glob.sync('styles/**/*.test.scss');
+
+  sassTestFiles.forEach(file => {
+    sassTrue.runSass({ describe, it }, file);
+  });
+});
